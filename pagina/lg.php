0ba2dfd43b526493c3f98d37ec838fe72ec32ba5
IrvinGalindo <irvin_its@outlook.es>
2018-08-12 01:35:17 -0500

    hgfds

--- pagina/lg.php
+++ pagina/lg.php
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-<?php
-    session_start();
-    include 'config.php';
-    //obtenemos las credenciales del usuario
-     $usr=$_REQUEST['id_usuario'];
-     $pwd=$_REQUEST['pwd'];
-     
-     if(empty($usr)){
-        $errors[]= "Ingresar un usuario";
-     }else{
-        if(empty($pwd)){
-            $errors[]= "Ingresar password";
-        }else{
-             //preparamos nuestra declaracion
-        $stmt = $pdo->prepare("SELECT tipo FROM usuario WHERE id_usuario=? AND pwd=?");
-        //ejecutamos el query haciendo que pdo reemplace las variables.
-        $stmt->execute(array($usr, $pwd));
-        //obtenemos los resultado
-        $resultado = $stmt -> fetchColumn(0);       
-       $resultado;
-        if(strcmp($resultado,"")!==0){
-           
-            if(strcmp($resultado,"0") === 0)
-            {
-                $_SESSION['usuario'] = $usr;
-             echo "<script>window.location.replace('adminPage.php')</script>";
-             }
-            else{
-                 $_SESSION['usuario'] = $usr;
-             echo "<script>window.location.replace('aCl.php')</script>";
-            }
-        }else{
-            $errors[]= "Usuario o password incorrectos";
-        }
-    }
-    }
-    if (isset($errors)){
-            
-            ?>
-            <div class="alert alert-danger" role="alert">
-                <button type="button" class="close" data-dismiss="alert">&times;</button>
-                    <strong>Error!</strong> 
-                    <?php
-                        foreach ($errors as $error) {
-                                echo $error;
-                            }
-                        ?>
-            </div>
-            <?php
-            }
-      
-=======
 <?php
     session_start();
     include 'config.php';
@@ -106,5 +53,4 @@
             <?php
             }
       
->>>>>>> 718cfcc362c0d487aab55a62156a4e6c25a2c596
 ?>
